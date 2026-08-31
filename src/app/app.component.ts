931cdeea0d033ccc037ff71c1e3789dcf08464c8
Ajnish <ajnsih@123>
2026-08-31 15:52:13 +0530

    change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,69 +1,3 @@
-// import { Component } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
-// import { CommonModule } from '@angular/common';
-// import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
-// import { Employee } from './Model/Employee';
-
-// @Component({
-//   selector: 'app-root',
-//   standalone: true,
-//   imports: [RouterOutlet, CommonModule, ReactiveFormsModule],
-//   templateUrl: './app.component.html',
-//   styleUrl: './app.component.css'
-// })
-// export class AppComponent {
-//   employeeForm!: FormGroup;
-//   employeeObj: Employee = new Employee();
-//   employeList: Employee[] = [];
-//   employeeList: Employee[] = [];
-//   isEditMode: boolean = false;
-
-//   constructor() {
-//     debugger;
-//     this.createForm();
-//     const oldData = localStorage.getItem('employeeList');
-//     if (oldData !== null) {
-//       this.employeeList = JSON.parse(oldData);
-//     }
-//   }
-
-//   createForm(): void {
-//     this.employeeForm = new FormGroup({
-//       employeeId: new FormControl(0),
-//       employeeName: new FormControl(this.employeeObj.employeeName),
-//       employeeEmail: new FormControl(this.employeeObj.employeeEmail),
-//       employeePhone: new FormControl(this.employeeObj.employeePhone),
-//       employeeaddress: new FormControl(this.employeeObj.employeeaddress),
-//       employeeSalary: new FormControl(this.employeeObj.employeeSalary)
-
-
-
-//     });
-//   }
-
-//   onSave(){
-//     debugger;
-//     const oldData=localStorage.getItem('employeeList');
-//     if(oldData!==null){
-//       const parsedData: Employee[] = JSON.parse(oldData);
-//       this.employeeForm.controls['employeeId'].setValue(parsedData.length + 1);
-//       this.employeeList.unshift(this.employeeForm.value);
-
-
-//     }else{
-//       this.employeeList.unshift(this.employeeForm.value);
-//     }
-
-
-//   }
-
-// }
-
-
-
-
-
-
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
@@ -84,7 +18,6 @@ export class AppComponent {
   isEditMode: boolean = false;
 
   constructor() {
-    debugger;
     this.createForm();
     const oldData = localStorage.getItem('employeeList');
     if (oldData !== null) {
@@ -95,16 +28,31 @@ export class AppComponent {
   createForm(): void {
     this.employeeForm = new FormGroup({
       employeeId: new FormControl(0),
-      employeeName: new FormControl(this.employeeObj.employeeName, [Validators.required]),
-      employeeEmail: new FormControl(this.employeeObj.employeeEmail, [Validators.required, Validators.email]),
-      employeePhone: new FormControl(this.employeeObj.employeePhone, [Validators.required]),
-      employeeaddress: new FormControl(this.employeeObj.employeeaddress, [Validators.required]),
-      employeeSalary: new FormControl(this.employeeObj.employeeSalary, [Validators.required])
+      employeeName: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      employeeEmail: new FormControl('', [Validators.required, Validators.email]),
+      employeePhone: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[0-9]{10}$')
+      ]),
+      employeeSalary: new FormControl('', [Validators.required, Validators.min(1)]),
+      employeeaddress: new FormControl('', [Validators.required])
     });
   }
 
-  onSave() {
-    debugger;
+  // Restricts user input strictly to 10 digits as they type
+  onPhoneInput(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    const sanitized = input.value.replace(/[^0-9]/g, '').slice(0, 10);
+    this.employeeForm.get('employeePhone')?.setValue(sanitized, { emitEvent: false });
+    input.value = sanitized;
+  }
+
+  onSave(): void {
+    if (this.employeeForm.invalid) {
+      this.employeeForm.markAllAsTouched();
+      return;
+    }
+
     const oldData = localStorage.getItem('employeeList');
     if (oldData !== null) {
       const parsedData: Employee[] = JSON.parse(oldData);
@@ -116,28 +64,33 @@ export class AppComponent {
       this.employeeList.unshift(this.employeeForm.value);
       localStorage.setItem('employeeList', JSON.stringify(this.employeeList));
     }
+
     this.onReset();
   }
 
-  onEdit(data: Employee) {
-    debugger;
+  onEdit(data: Employee): void {
     this.isEditMode = true;
     this.employeeForm.patchValue(data);
   }
 
-  onUpdate() {
-    debugger;
+  onUpdate(): void {
+    if (this.employeeForm.invalid) {
+      this.employeeForm.markAllAsTouched();
+      return;
+    }
+
     const updatedData: Employee = this.employeeForm.value;
     const index = this.employeeList.findIndex(e => e.employeeId === updatedData.employeeId);
+
     if (index !== -1) {
       this.employeeList[index] = updatedData;
       localStorage.setItem('employeeList', JSON.stringify(this.employeeList));
     }
+
     this.onReset();
   }
 
-  onDelete(id: number) {
-    debugger;
+  onDelete(id: number): void {
     const isConfirm = confirm('Are you sure you want to delete this employee?');
     if (isConfirm) {
       this.employeeList = this.employeeList.filter(e => e.employeeId !== id);
@@ -148,15 +101,15 @@ export class AppComponent {
     }
   }
 
-  onReset() {
+  onReset(): void {
     this.isEditMode = false;
     this.employeeForm.reset({
       employeeId: 0,
       employeeName: '',
       employeeEmail: '',
       employeePhone: '',
-      employeeaddress: '',
-      employeeSalary: ''
+      employeeSalary: '',
+      employeeaddress: ''
     });
   }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Employee } from './Model/Employee';
 
 export const routes: Routes = [];
+
+
+
